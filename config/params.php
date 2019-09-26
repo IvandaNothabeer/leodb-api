cf8a8fb1d163e1e11519adfd7e5a4dda60f67ac8
IvandaNothabeer <kossaw.eve@gmail.com>
2019-09-26 16:04:15 +0200

    set email info

--- config/params.php
+++ config/params.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'adminEmail' => 'ahakroon1@gmail.com',
+    'adminEmail' => 'ahakroon@hotmail.com',
     'senderEmail' => 'ben.kroon@leonberger-database.com',
     'senderName' => 'Leonberger API mailer',
 ];
